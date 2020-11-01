781f2793456180cff125862eb344970e9dd86be2
dom <69329669+auzxo@users.noreply.github.com>
2020-11-01 01:51:23 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# icon-changer
+# League of Legends Hidden Summoner Icon Changer
+
+This little application made from Python using tkinter and the Riot LCU API allows you to change your summoner icon to any of the hidden chibi icons
+
+## How to install 
+[Click Here To Download Application](https://github.com/MManoah/lol-summoner-icon-changer/releases/download/V.2.2.1/Summoner.Icon.Changer.zip)
+then extract all the files somewhere and run Summoner Icon Changer.exe
+
+[Releases](https://github.com/MManoah/lol-summoner-icon-changer/releases/)
+
+## Project Overview
+**Just click on the desired icon**
+
+![](https://i.gyazo.com/649770498439431650abe138ccca2dfe.png)
+
+## Built With
+
+* [Lcu Driver](https://github.com/sousa-andre/lcu-driver) - Accessing the LCU API
+* Python
+
+## Riot Games
+
+This project isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games
+or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are
+trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ Summoner Icon Changer.py
@@ -0,0 +1,101 @@
+import os
+from os.path import isfile, join
+from tkinter import *
+from tkinter import messagebox
+import psutil
+from PIL import Image, ImageTk
+from json import dumps
+from lcu_driver import Connector
+
+Window_Width = 650
+Window_Height = 475
+connector = Connector()
+global SummonerIcon
+
+
+# Makes a request to set the proper icon by the icon code
+def set_icon_button(entry):
+    global SummonerIcon
+    SummonerIcon = entry
+
+    async def set_icon():
+        response = await connector.request('put', '/lol-summoner/v1/current-summoner/icon',
+                                           data=dumps({'profileIconId': SummonerIcon}))
+        if response.status == 201:
+            messagebox.showinfo("Status", "SUCCESS!")
+        else:
+            messagebox.showerror("Error", "THERE WAS AN ERROR")
+        await connector.stop_ws()
+
+    @connector.event
+    async def connect():
+        await set_icon()
+
+    connector.start()
+
+
+class MainWindow:
+    def __init__(self, master):
+        image_list = None
+        self.master = master
+        master.title('Summoner Icon Changer - Manoah')
+        master.iconbitmap('Background and Icon/Icon.ico')
+        canvas = Canvas(master, height=Window_Height, width=Window_Width)
+        canvas.pack()
+
+        img = Image.open("Background and Icon/Background.jpg")
+        img = img.resize((864, 486), Image.ANTIALIAS)
+        background_image = ImageTk.PhotoImage(img)
+        background = Label(master, image=background_image)
+        background.image = background_image
+        background.place(relwidth=1, relheight=1)
+        path = 'Summoner Icon Pictures'  # Summoner Icon Pictures folder
+        try:
+            image_list = [f for f in os.listdir(path) if isfile(join(path, f))]  # Creates a list of the pictures
+        except:
+            messagebox.showerror("Error", "SUMMONER ICON PICTURES COULD NOT BE FOUND")
+            exit(1)
+        relx = 0.05
+        rely = 0.05
+        for i in range(len(image_list)):
+            icon_button = image_list[i]
+            icon_button = int(icon_button[:-4])
+            placement("{path}/{x}".format(path=path, x=image_list[i]), background, icon_button, relx, rely)
+            if (i + 1) % 5 == 0:  # 5 is the number of columns
+                relx = 0.05
+            else:
+                relx += 0.20
+            if (i + 1) % 5 == 0:  # When 5 icons have been placed move to the next row
+                rely += 0.15
+
+
+# Places icons on the label called background
+def placement(path, background, icon_button, x, y):
+    image = Image.open(path)
+    image = image.resize((50, 50), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(image)
+    button = Button(background, image=image, borderwidth=0, highlightthickness=0,
+                    command=lambda: set_icon_button(icon_button))
+    button.image = image
+    button.place(relx=x, rely=y)
+
+
+# Checks if the league client is open and logged in
+def ProcessExists():
+    for p in psutil.process_iter():
+        try:
+            if p.name() == 'LeagueClient.exe':
+                return True
+        except psutil.Error:
+            return False
+
+
+if __name__ == '__main__':
+    if ProcessExists():
+        root = Tk()
+        root.resizable(0, 0)
+        application = MainWindow(root)
+        root.mainloop()
+    else:
+        messagebox.showerror("Error", "PLEASE LOGIN TO LEAGUE OF LEGENDS")
+        exit(1)
